22fbe3e3a638c0c315c2306af42713e1b2e6d9c3
bigeagle <justin.w.xd@gmail.com>
2014-10-13 11:33:35 +0800

    danmaku color

--- danmaku_ui.py
+++ danmaku_ui.py
@@ -3,11 +3,26 @@
 from random import randint
 import cairo
 from gi.repository import Gtk, GObject
+from threading import Lock
+
+color_styles = {
+    "white": ((1, 1, 1), (0, 0, 0), ),
+    "blue": ((20/255.0, 95/255.0, 198/255.0), (1, 1, 1), ),
+    "lightblue": ((100/255.0, 172/255.0, 239/255.0), (0, 0, 0), ),
+    "cyan": ((0, 1, 1), (0, 0, 0), ),
+    "red": ((231/255.0, 34/255.0, 0), (1, 1, 1), ),
+    "yellow": ((1, 221/255.0, 0), (0, 0, 0), ),
+    "green": ((4/255.0, 202/255.0, 0), (0, 0, 0), ),
+}
 
 
 class Danmaku(Gtk.Window):
+    _lock = Lock()
+    count = 0
+    bottom_count = 0
+    top_count = 0
 
-    def __init__(self, text=u"哈哈哈"):
+    def __init__(self, text=u"我来组成弹幕", style="white", position="fly"):
         super(Danmaku, self).__init__(
             type=Gtk.WindowType.POPUP, title="Danmaku")
         # super(Danmaku, self).__init__(title="Danmaku")
@@ -17,6 +32,18 @@ class Danmaku(Gtk.Window):
         # self.set_decorated(False)
         # self.set_size_request(500, 40)
         # self.set_size_request(370, 240)
+
+        self.style = style
+        self.position = position
+
+        with Danmaku._lock:
+            Danmaku.count += 1
+            if self.position == "bottom":
+                Danmaku.bottom_count += 1
+            elif self.position == "top":
+                Danmaku.top_count += 1
+
+            print(self.count)
 
         self.screen = self.get_screen()
         self.visual = self.screen.get_rgba_visual()
@@ -39,23 +66,30 @@ class Danmaku(Gtk.Window):
         self.show_all()
 
     def clear_background(self, widget, cr):
-        cr.set_source_rgba(0.9, 0.9, 0.9, 0)
+        cr.set_source_rgba(0, 0, 0, 0)
         cr.set_operator(cairo.OPERATOR_SOURCE)
         cr.paint()
         cr.set_operator(cairo.OPERATOR_OVER)
 
     def draw_text(self, widget, cr):
-        cr.set_source_rgba(0.9, 0.9, 0.9, 0)
+        cr.set_source_rgba(0, 0, 0, 0)
         cr.set_operator(cairo.OPERATOR_SOURCE)
         cr.paint()
         cr.set_operator(cairo.OPERATOR_OVER)
-        cr.set_source_rgb(0.9, 0.9, 0.9)
-        cr.select_font_face("STHeiti", cairo.FONT_SLANT_NORMAL,
+
+        font_color, border_color = color_styles.get(
+            self.style,
+            color_styles["white"]
+        )
+
+        cr.set_source_rgb(*font_color)
+        cr.select_font_face("WenQuanYi Micro Hei",
+                            cairo.FONT_SLANT_NORMAL,
                             cairo.FONT_WEIGHT_BOLD)
         cr.set_font_size(24)
         cr.move_to(20, 30)
         cr.show_text(self.text)
-        cr.set_source_rgb(0, 0, 0)
+        cr.set_source_rgb(*border_color)
         cr.move_to(20, 30)
         cr.set_line_width(0.5)
         cr.text_path(self.text)
@@ -65,9 +99,15 @@ class Danmaku(Gtk.Window):
 
     def fly(self):
         self.move(self.x, self.y)
-        self.x -= 3
+        self.x -= 30
         if self.x < -400:
             self.destroy()
+            with Danmaku._lock:
+                Danmaku.count -= 1
+            if self.position == "bottom":
+                Danmaku.bottom_count -= 1
+            elif self.position == "top":
+                Danmaku.top_count -= 1
 
         GObject.timeout_add(30, self.fly)
 
@@ -75,6 +115,7 @@ class Danmaku(Gtk.Window):
 if __name__ == "__main__":
 
     Danmaku("我爱bilibili")
+    Danmaku("Test", style="darkblue")
     Gtk.main()
 
 # vim: ts=4 sw=4 sts=4 expandtab
