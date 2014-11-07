34cec8b750eeebcc17bb435e8b4f7dbe6bdbed7f
bigeagle <justin.w.xd@gmail.com>
2014-11-07 22:44:16 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+# -*- coding:utf-8 -*-
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.platform == "win32":
+    import py2exe
+
+
+packages = ["gdanmaku"]
+requires = ["requests"]
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="gdanmaku",
+    version="0.2-dev",
+    description="Display danmaku on any screen",
+    long_description=readme,
+    author="Justin Wong",
+    author_email="justin.w.xd@gmail.com",
+    url="https://github.com/tuna/gdanmaku/",
+    packages=packages,
+    package_data={'': ['LICENCE', ], 'gdanmaku': [os.path.join('images', '*.png'), ]},
+    package_dir={'gdanmaku': 'gdanmaku'},
+    include_package_data=True,
+    install_requires=requires,
+    license="GPLv3",
+    entry_points={
+        'console_scripts': ['gdanmaku = gdanmaku.danmaku:main'],
+    },
+)
+
+# vim: ts=4 sw=4 sts=4 expandtab
